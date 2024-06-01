45f8f7f6826b03cda6c6c4869591d3cd2e858175
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-06-01 10:54:53 -0300

    feat: add Database,Cloudinary and Redis Connection

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,9 @@
 import { app } from "./app";
+import connectDB from "./utils/db";
 
 require("dotenv").config();
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
+  connectDB()
 });
