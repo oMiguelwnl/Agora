6c24a0ac33ab19f027ecb5db6b147f67a0014990
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-06-02 19:26:42 -0300

    Feat: Add User Registration

--- server/app.ts
+++ server/app.ts
@@ -4,6 +4,7 @@ export const app = express();
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import { ErrorMiddleware } from "./middleware/error";
+import userRouter from "./routes/user.route";
 
 // body parser
 app.use(express.json({ limit: "50mb" }));
@@ -13,6 +14,9 @@ app.use(cookieParser());
 
 // cors
 app.use(cors({ origin: process.env.ORIGIN }));
+
+// routes
+app.use("/api/v1", userRouter);
 
 // testing api
 app.get("/test", (req: Request, res: Response, next: NextFunction) => {
