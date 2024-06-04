ee7d3df6ed0b810a131a3d74a04bf56239f286d1
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-06-04 09:51:26 -0300

    feat: add Logout User functionality

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -157,3 +157,22 @@ export const loginUser = CatchAsyncError(
     }
   }
 );
+
+// Logout User
+
+export const logoutUser = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      res.cookie("access_token", "", { maxAge: 1 });
+      res.cookie("refresh_token", "", { maxAge: 1 });
+
+      res.status(200).json({
+        success:true,
+        message: "Logged out successfully"
+      })
+
+    } catch (error: any) {
+      return next(new ErrorHandler("Please enter email and password", 400));
+    }
+  }
+);
--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -2,6 +2,7 @@ import express from "express";
 import {
   activateUser,
   loginUser,
+  logoutUser,
   registrationUser,
 } from "../controllers/user.controller";
 const userRouter = express.Router();
@@ -11,5 +12,6 @@ userRouter.post("/registration", registrationUser);
 userRouter.post("/activate-user", activateUser);
 
 userRouter.post("/login", loginUser);
+userRouter.get("/logout", logoutUser);
 
 export default userRouter;
