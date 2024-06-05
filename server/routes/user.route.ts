13cc842683f69a236f101099906256588b8e9e0f
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-06-05 15:41:04 -0300

    feat: add update user info route

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -7,6 +7,7 @@ import {
   registrationUser,
   socialAuth,
   updateAccessToken,
+  updateUserInfo,
 } from "../controllers/user.controller";
 import { isAuthenticated } from "../middleware/auth";
 const userRouter = express.Router();
@@ -24,5 +25,7 @@ userRouter.get("/refresh", updateAccessToken);
 userRouter.get("/me", isAuthenticated, getUserInfo);
 
 userRouter.post("/social-auth", socialAuth);
+
+userRouter.put("/update-user-info", isAuthenticated, updateUserInfo);
 
 export default userRouter;
--- server/services/user.service.ts
+++ server/services/user.service.ts
@@ -1,11 +1,14 @@
 import { Response } from "express";
-import userModel from "../models/user.model";
+import { redis } from "../utils/redis";
 
 export const getUserById = async (id: string, res: Response) => {
-  const user = await userModel.findById(id);
+  const userJson = await redis.get(id);
 
-  res.status(201).json({
-    success: true,
-    user,
-  });
+  if (userJson) {
+    const user = JSON.parse(userJson);
+    res.status(201).json({
+      success: true,
+      user,
+    });
+  }
 };
