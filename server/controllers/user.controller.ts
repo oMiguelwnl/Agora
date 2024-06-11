035f2fecb4add26394eef6b490644c5d9a055457
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-06-11 15:17:00 -0300

    feat: add Advance Cache Maintenance

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -201,7 +201,9 @@ export const updateAccessToken = CatchAsyncError(
       const session = await redis.get(decoded.id as string);
 
       if (!session) {
-        return next(new ErrorHandler(message, 400));
+        return next(
+          new ErrorHandler("Please login for access this resources!", 400)
+        );
       }
 
       const user = JSON.parse(session);
@@ -226,6 +228,8 @@ export const updateAccessToken = CatchAsyncError(
 
       res.cookie("access_token", accessToken, accessTokenOptions);
       res.cookie("refresh_token", refreshToken, refreshTokenOptions);
+
+      await redis.set(user._id, JSON.stringify(user), "EX", 604800);
 
       res.status(200).json({
         status: "success",
