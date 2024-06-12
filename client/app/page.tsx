0610095ea9c4eb39c3a86aee610d2624c9e0748f
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-06-12 10:25:46 -0300

    feat: add Theme Provider

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,5 +1,20 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
-  return;
-}
+import React, { FC, useState } from "react";
+import Heading from "./utils/Heading";
+
+interface Props {}
+
+const Page: FC<Props> = (props) => {
+  return (
+    <>
+      <Heading
+        title="Ágora"
+        description="Ágora is a platform for students to learn and get help from teachers"
+        keywords="Programming, MERN, Courses, Machine Learning, "
+      />
+    </>
+  );
+};
+
+export default Page;
