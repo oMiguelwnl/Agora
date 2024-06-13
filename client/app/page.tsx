abd592d20201a238355e0f8c86d5538907ddfc69
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-06-13 15:23:19 -0300

    feat: add Hero component

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -3,6 +3,7 @@
 import React, { FC, useState } from "react";
 import Heading from "./utils/Heading";
 import Header from "./components/Header";
+import Hero from "./components/Route/Hero";
 
 interface Props {}
 
@@ -17,6 +18,7 @@ const Page: FC<Props> = (props) => {
         keywords="Programming, MERN, Courses, Machine Learning, "
       />
       <Header open={open} setOpen={setOpen} activeItem={activeItem} />
+      <Hero />
     </div>
   );
 };
