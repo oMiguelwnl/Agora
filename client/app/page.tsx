ae8ea997b2bbebaded4d0a269b5a0109f10cf8ca
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-06-13 11:46:07 -0300

    feat: add Header component

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -2,18 +2,22 @@
 
 import React, { FC, useState } from "react";
 import Heading from "./utils/Heading";
+import Header from "./components/Header";
 
 interface Props {}
 
 const Page: FC<Props> = (props) => {
+  const [open, setOpen] = useState(false);
+  const [activeItem, setActiveItem] = useState(0);
   return (
-    <>
+    <div>
       <Heading
         title="Ágora"
         description="Ágora is a platform for students to learn and get help from teachers"
         keywords="Programming, MERN, Courses, Machine Learning, "
       />
-    </>
+      <Header open={open} setOpen={setOpen} activeItem={activeItem} />
+    </div>
   );
 };
 
