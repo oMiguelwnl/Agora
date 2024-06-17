639bee06b3944a21e3b67db050ebb2a761c21f3a
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-06-17 10:59:42 -0300

    feat: Add verification page to header

--- client/app/components/Header.tsx
+++ client/app/components/Header.tsx
@@ -7,6 +7,7 @@ import NavItems from "../utils/NavItem";
 import CustomModal from "../utils/CustomModal";
 import Login from "../components/Auth/Login";
 import SignUp from "../components/Auth/SignUp";
+import Verification from "../components/Auth/Verification";
 
 type Props = {
   open: boolean;
@@ -39,95 +40,116 @@ const Header: FC<Props> = ({ activeItem, setOpen, route, open, setRoute }) => {
   };
 
   return (
-    <div className="w-full relative">
-      <div
-        className={`${
-          active
-            ? "dark:bg-opacity-50 bg-white dark:bg-gradient-to-b dark:from-gray-900 dark:to-black fixed top-0 left-0 w-full h-[80px] z-[80] border-b dark:border-[#ffffff1c] shadow-xl transition duration-500"
-            : "w-full border-b dark:border-[#ffffff1c] h-[80px] z-[80] dark:shadow"
-        }`}
-      >
-        <div className="w-[95%] 800px:w-[92%] m-auto py-2 h-full">
-          <div className="w-full h-[80px] flex items-center justify-between p-3">
-            <div>
-              <Link
-                href={"/"}
-                className="text-[25px] font-Poppins font-[500] text-black dark:text-white"
-              >
-                Ágora
-              </Link>
-            </div>
-            <div className="flex items-center">
-              <NavItems activeItem={activeItem} isMobile={false} />
-              <ThemeSwitcher />
-              {/* Only from mobile */}
-              <div className="800px:hidden">
-                <HiOutlineMenuAlt3
-                  size={25}
-                  className="cursor-pointer dark:text-white text-black"
-                  onClick={() => setOpenSidebar(true)}
-                />
-              </div>
-              <HiOutlineUserCircle
-                size={25}
-                className="hidden 8000px:block cursor-pointer dark:text-white text-black"
-                onClick={() => setOpen(true)}
-              />
-            </div>
-          </div>
-        </div>
-        {/* mobile Sidebar */}
-        {openSidebar && (
+    <>
+      {
+        <div className="w-full relative">
           <div
-            className="fixed w-full h-screen top-0 left-0 z-[99999] dark:bg-[unset] bg-[#00000024]"
-            onClick={handleClose}
-            id="screen"
+            className={`${
+              active
+                ? "dark:bg-opacity-50 bg-white dark:bg-gradient-to-b dark:from-gray-900 dark:to-black fixed top-0 left-0 w-full h-[80px] z-[80] border-b dark:border-[#ffffff1c] shadow-xl transition duration-500"
+                : "w-full border-b dark:border-[#ffffff1c] h-[80px] z-[80] dark:shadow"
+            }`}
           >
-            <div className="w-[70%] fixed z-[999999999] h-screen bg-white dark:bg-slate-900 dark:bg-opacity-90 top-0 right-0">
-              <NavItems activeItem={activeItem} isMobile={true} />
-              <HiOutlineUserCircle
-                size={25}
-                className="cursor-pointer ml-5 my-2"
-                onClick={() => setOpen(true)}
-              />
-              <br />
-              <br />
-              <p className="text-[16px] px-2 pl-5 text-black dark:text-white">
-                Copyright © 2024 Ágora{" "}
-              </p>
+            <div className="w-[95%] 800px:w-[92%] m-auto py-2 h-full">
+              <div className="w-full h-[80px] flex items-center justify-between p-3">
+                <div>
+                  <Link
+                    href={"/"}
+                    className={`text-[25px] font-Poppins font-[500] text-black dark:text-white`}
+                  >
+                    Ágora
+                  </Link>
+                </div>
+                <div className="flex items-center">
+                  <NavItems activeItem={activeItem} isMobile={false} />
+                  <ThemeSwitcher />
+                  {/* only for mobile */}
+                  <div className="800px:hidden">
+                    <HiOutlineMenuAlt3
+                      size={25}
+                      className="cursor-pointer dark:text-white text-black"
+                      onClick={() => setOpenSidebar(true)}
+                    />
+                  </div>
+
+                  <HiOutlineUserCircle
+                    size={25}
+                    className="hidden 800px:block cursor-pointer dark:text-white text-black"
+                    onClick={() => setOpen(true)}
+                  />
+                </div>
+              </div>
             </div>
+
+            {/* mobile sidebar */}
+            {openSidebar && (
+              <div
+                className="fixed w-full h-screen top-0 left-0 z-[99999] dark:bg-[unset] bg-[#00000024]"
+                onClick={handleClose}
+                id="screen"
+              >
+                <div className="w-[70%] fixed z-[999999999] h-screen bg-white dark:bg-slate-900 dark:bg-opacity-90 top-0 right-0">
+                  <NavItems activeItem={activeItem} isMobile={true} />
+
+                  <HiOutlineUserCircle
+                    size={25}
+                    className="hidden 800px:block cursor-pointer dark:text-white text-black"
+                    onClick={() => setOpen(true)}
+                  />
+
+                  <br />
+                  <br />
+                  <p className="text-[16px] px-2 pl-5 text-black dark:text-white">
+                    Copyright © 2024 Ágora
+                  </p>
+                </div>
+              </div>
+            )}
           </div>
-        )}
-      </div>
-
-      {route === "Login" && (
-        <>
-          {open && (
-            <CustomModal
-              open={open}
-              setOpen={setOpen}
-              setRoute={setRoute}
-              activeItem={activeItem}
-              component={Login}
-            />
+          {route === "Login" && (
+            <>
+              {open && (
+                <CustomModal
+                  open={open}
+                  setOpen={setOpen}
+                  setRoute={setRoute}
+                  activeItem={activeItem}
+                  component={Login}
+                />
+              )}
+            </>
           )}
-        </>
-      )}
 
-      {route === "Sign-Up" && (
-        <>
-          {open && (
-            <CustomModal
-              open={open}
-              setOpen={setOpen}
-              setRoute={setRoute}
-              activeItem={activeItem}
-              component={SignUp}
-            />
+          {route === "Sign-Up" && (
+            <>
+              {open && (
+                <CustomModal
+                  open={open}
+                  setOpen={setOpen}
+                  setRoute={setRoute}
+                  activeItem={activeItem}
+                  component={SignUp}
+                />
+              )}
+            </>
           )}
-        </>
-      )}
-    </div>
+
+          {route === "Verification" && (
+            <>
+              {open && (
+                <CustomModal
+                  open={open}
+                  setOpen={setOpen}
+                  setRoute={setRoute}
+                  activeItem={activeItem}
+                  component={Verification}
+                />
+              )}
+            </>
+          )}
+        </div>
+      }
+    </>
   );
 };
 
--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,8 +1,8 @@
 import "./globals.css";
-import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import { Josefin_Sans } from "next/font/google";
 import { ThemeProvider } from "./utils/theme-provider";
+import { Toaster } from "react-hot-toast";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -28,6 +28,7 @@ export default function RootLayout({
       >
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
+          <Toaster position="top-center" reverseOrder={false} />
         </ThemeProvider>
       </body>
     </html>
