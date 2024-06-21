b8671da1da08b8a7f062b9c829b74ebacdd3fcc4
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-06-21 11:48:31 -0300

    feat(auth): Protect routes with user authentication

---
+++ client/app/hooks/useProtected.tsx
@@ -0,0 +1,13 @@
+import { redirect } from "next/navigation";
+import userAuth from "./userAuth";
+import React from "react";
+
+interface ProtectedProps {
+  children: React.ReactNode;
+}
+
+export default function Protected({ children }: ProtectedProps) {
+  const isAuthenticated = userAuth();
+
+  return isAuthenticated ? children : redirect("/");
+}
