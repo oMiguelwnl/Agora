4a91a027fc94f3c05dded48728aa9c7b278a7859
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-06-21 11:50:24 -0300

    feat(ui): Implement user profile page

---
+++ client/app/profile/page.tsx
@@ -0,0 +1,37 @@
+"use client";
+import React, { FC, useState } from "react";
+import Protected from "../hooks/useProtected";
+import Heading from "../utils/Heading";
+import Header from "../components/Header";
+import { useSelector } from "react-redux";
+import Profile from "../components/Profile";
+
+type Props = {};
+
+const page: FC<Props> = (props) => {
+  const [open, setOpen] = useState(false);
+  const [activeItem, setActiveItem] = useState(0);
+  const [route, setRoute] = useState("Login");
+  const { user } = useSelector((state: any) => state.auth);
+  return (
+    <div>
+      <Protected>
+        <Heading
+          title={`${user?.name} profile`}
+          description="Ágora is a platform for students to learn and get help from teachers"
+          keywords="Programming, MERN, Courses, Machine Learning, "
+        />
+        <Header
+          open={open}
+          setOpen={setOpen}
+          activeItem={activeItem}
+          setRoute={setRoute}
+          route={route}
+        />
+        <Profile user={user} />
+      </Protected>
+    </div>
+  );
+};
+
+export default page;
