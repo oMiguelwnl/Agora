2f4f873f979671abc16c12032ac8e5c251368c0c
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-06-21 11:52:18 -0300

    feat(ui): Create Profile component

---
+++ client/app/components/Profile.tsx
@@ -0,0 +1,44 @@
+import React, { FC, useState } from "react";
+import SideBarProfile from "./SideBarProfile";
+
+type Props = {
+  user: any;
+};
+
+const Profile: FC<Props> = ({ user }) => {
+  const [scroll, setScroll] = useState(false);
+  const [avatar, setAvatar] = useState(null);
+  const [active, setActive] = useState(1);
+
+  const logOutHandler = async () => {};
+
+  if (typeof window !== "undefined") {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 85) {
+        setScroll(true);
+      } else {
+        setScroll(false);
+      }
+    });
+  }
+
+  return (
+    <div className="w-[85%] flex mx-auto">
+      <div
+        className={`w-[60px] 800px:w-[310px] h-[450px] dark:bg-slate-900 bg-opacity-90 border bg-white dark:border-[#ffffff1d] border-[#00000014] rounded-[5px] shadow-sm dark:shadow-sm mt-[80px] mb-[80px] sticky ${
+          scroll ? "top-[120px]" : "top-[30px]"
+        } left-[30px]`}
+      >
+        <SideBarProfile
+          user={user}
+          active={active}
+          avatar={avatar}
+          setActive={setActive}
+          logOutHandler={logOutHandler}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
