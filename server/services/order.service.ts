8d846552709f7c3337b6f634de103a4c5c5e4758
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-06-27 10:35:26 -0300

    fix(orders): change sort field to createdAt in getAllOrdersService

--- server/services/order.service.ts
+++ server/services/order.service.ts
@@ -2,18 +2,17 @@ import { NextFunction, Response } from "express";
 import { CatchAsyncError } from "../middleware/catchAsyncErrors";
 import OrderModel from "../models/order.model";
 
-export const newOrder = CatchAsyncError(
-  async (data: any, next: NextFunction, res: Response) => {
-    const order = await OrderModel.create(data);
-    res.status(200).json({
-      success: true,
-      order,
-    });
-  }
-);
+export const newOrder = CatchAsyncError(async (data: any, res: Response) => {
+  const order = await OrderModel.create(data);
 
-export const geAllOrdersService = async (res: Response) => {
-  const orders = await OrderModel.find().sort({ created: -1 });
+  res.status(201).json({
+    success: true,
+    order,
+  });
+});
+
+export const getAllOrdersService = async (res: Response) => {
+  const orders = await OrderModel.find().sort({ createdAt: -1 });
 
   res.status(201).json({
     success: true,
--- server/utils/ErrorHandler.ts
+++ server/utils/ErrorHandler.ts
@@ -2,10 +2,9 @@ class ErrorHandler extends Error {
   statusCode: Number;
   constructor(message: any, statusCode: Number) {
     super(message);
-
     this.statusCode = statusCode;
     Error.captureStackTrace(this, this.constructor);
   }
 }
 
-export default ErrorHandler
+export default ErrorHandler;
