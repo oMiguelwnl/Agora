3303ffc44410730fb5f1a46ad15dcd550da1c927
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-06-27 11:20:13 -0300

    refactor(header): optimize imports and enhance session management

--- client/app/components/Header.tsx
+++ client/app/components/Header.tsx
@@ -1,14 +1,13 @@
 "use client";
 import Link from "next/link";
 import React, { FC, useEffect, useState } from "react";
+import NavItems from "../utils/NavItems";
 import { ThemeSwitcher } from "../utils/ThemeSwitcher";
 import { HiOutlineMenuAlt3, HiOutlineUserCircle } from "react-icons/hi";
-import NavItems from "../utils/NavItem";
 import CustomModal from "../utils/CustomModal";
 import Login from "../components/Auth/Login";
 import SignUp from "../components/Auth/SignUp";
 import Verification from "../components/Auth/Verification";
-import { useSelector } from "react-redux";
 import Image from "next/image";
 import avatar from "../../public/assets/avatar.png";
 import { useSession } from "next-auth/react";
@@ -16,7 +15,9 @@ import {
   useLogOutQuery,
   useSocialAuthMutation,
 } from "@/redux/features/auth/authApi";
-import toast from "react-hot-toast";
+import { toast } from "react-hot-toast";
+import { useLoadUserQuery } from "@/redux/features/api/apiSlice";
+import Loader from "./Loader/Loader";
 
 type Props = {
   open: boolean;
@@ -29,7 +30,11 @@ type Props = {
 const Header: FC<Props> = ({ activeItem, setOpen, route, open, setRoute }) => {
   const [active, setActive] = useState(false);
   const [openSidebar, setOpenSidebar] = useState(false);
-  const { user } = useSelector((state: any) => state.auth);
+  const {
+    data: userData,
+    isLoading,
+    refetch,
+  } = useLoadUserQuery(undefined, {});
   const { data } = useSession();
   const [socialAuth, { isSuccess, error }] = useSocialAuthMutation();
   const [logout, setLogout] = useState(false);
@@ -38,26 +43,27 @@ const Header: FC<Props> = ({ activeItem, setOpen, route, open, setRoute }) => {
   });
 
   useEffect(() => {
-    if (!user) {
-      if (data) {
-        socialAuth({
-          email: data?.user?.email,
-          name: data?.user?.name,
-          avatar: data.user?.image,
-        });
+    if (!isLoading) {
+      if (!userData) {
+        if (data) {
+          socialAuth({
+            email: data?.user?.email,
+            name: data?.user?.name,
+            avatar: data.user?.image,
+          });
+          refetch();
+        }
+      }
+      if (data === null) {
+        if (isSuccess) {
+          toast.success("Login Successfully");
+        }
+      }
+      if (data === null && !isLoading && !userData) {
+        setLogout(true);
       }
     }
-
-    if (data === null) {
-      if (isSuccess) {
-        toast.success("Login Successfully");
-      }
-    }
-
-    if (data === null) {
-      setLogout(true);
-    }
-  }, [data, user]);
+  }, [data, userData, isLoading]);
 
   if (typeof window !== "undefined") {
     window.addEventListener("scroll", () => {
@@ -78,126 +84,156 @@ const Header: FC<Props> = ({ activeItem, setOpen, route, open, setRoute }) => {
   };
 
   return (
-    <div className="w-full relative">
-      <div
-        className={`${
-          active
-            ? "dark:bg-opacity-50 bg-white dark:bg-gradient-to-b dark:from-gray-900 dark:to-black fixed top-0 left-0 w-full h-[80px] z-[80] border-b dark:border-[#ffffff1c] shadow-xl transition duration-500"
-            : "w-full border-b dark:border-[#ffffff1c] h-[80px] z-[80] dark:shadow"
-        }`}
-      >
-        <div className="w-[95%] 800px:w-[92%] m-auto py-2 h-full">
-          <div className="w-full h-[80px] flex items-center justify-between p-3">
-            <div>
-              <Link
-                href={"/"}
-                className={`text-[25px] font-Poppins font-[500] text-black dark:text-white`}
-              >
-                Ágora
-              </Link>
-            </div>
-            <div className="flex items-center">
-              <NavItems activeItem={activeItem} isMobile={false} />
-              <ThemeSwitcher />
-              {/* only for mobile */}
-              <div className="800px:hidden">
-                <HiOutlineMenuAlt3
-                  size={25}
-                  className="cursor-pointer dark:text-white text-black"
-                  onClick={() => setOpenSidebar(true)}
-                />
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div className="w-full relative">
+          <div
+            className={`${
+              active
+                ? "dark:bg-opacity-50 bg-white dark:bg-gradient-to-b dark:from-gray-900 dark:to-black fixed top-0 left-0 w-full h-[80px] z-[80] border-b dark:border-[#ffffff1c] shadow-xl transition duration-500"
+                : "w-full border-b dark:border-[#ffffff1c] h-[80px] z-[80] dark:shadow"
+            }`}
+          >
+            <div className="w-[95%] 800px:w-[92%] m-auto py-2 h-full">
+              <div className="w-full h-[80px] flex items-center justify-between p-3">
+                <div>
+                  <Link
+                    href={"/"}
+                    className={`text-[25px] font-Poppins font-[500] text-black dark:text-white`}
+                  >
+                    Ágora
+                  </Link>
+                </div>
+                <div className="flex items-center">
+                  <NavItems activeItem={activeItem} isMobile={false} />
+                  <ThemeSwitcher />
+                  {/* only for mobile */}
+                  <div className="800px:hidden">
+                    <HiOutlineMenuAlt3
+                      size={25}
+                      className="cursor-pointer dark:text-white text-black"
+                      onClick={() => setOpenSidebar(true)}
+                    />
+                  </div>
+                  {userData ? (
+                    <Link href={"/profile"}>
+                      <Image
+                        src={
+                          userData?.user.avatar
+                            ? userData.user.avatar.url
+                            : avatar
+                        }
+                        alt=""
+                        width={30}
+                        height={30}
+                        className="w-[30px] h-[30px] rounded-full cursor-pointer"
+                        style={{
+                          border:
+                            activeItem === 5 ? "2px solid #37a39a" : "none",
+                        }}
+                      />
+                    </Link>
+                  ) : (
+                    <HiOutlineUserCircle
+                      size={25}
+                      className="hidden 800px:block cursor-pointer dark:text-white text-black"
+                      onClick={() => setOpen(true)}
+                    />
+                  )}
+                </div>
               </div>
-              {user ? (
-                <Link href={"/profile"}>
-                  <Image
-                    src={user.avatar ? user.avatar.url : avatar}
-                    alt="profile image"
-                    width={30}
-                    height={30}
-                    className="w-[30px] h-[30px] rounded-full cursor-pointer"
-                    style={{
-                      border: activeItem === 5 ? "2px solid #37a39a" : "none",
-                    }}
-                  />
-                </Link>
-              ) : (
-                <HiOutlineUserCircle
-                  size={25}
-                  className="hidden 800px:block cursor-pointer dark:text-white text-black"
-                  onClick={() => setOpen(true)}
+            </div>
+
+            {/* mobile sidebar */}
+            {openSidebar && (
+              <div
+                className="fixed w-full h-screen top-0 left-0 z-[99999] dark:bg-[unset] bg-[#00000024]"
+                onClick={handleClose}
+                id="screen"
+              >
+                <div className="w-[70%] fixed z-[999999999] h-screen bg-white dark:bg-slate-900 dark:bg-opacity-90 top-0 right-0">
+                  <NavItems activeItem={activeItem} isMobile={true} />
+                  {userData?.user ? (
+                    <Link href={"/profile"}>
+                      <Image
+                        src={
+                          userData?.user.avatar
+                            ? userData.user.avatar.url
+                            : avatar
+                        }
+                        alt=""
+                        width={30}
+                        height={30}
+                        className="w-[30px] h-[30px] rounded-full ml-[20px] cursor-pointer"
+                        style={{
+                          border:
+                            activeItem === 5 ? "2px solid #37a39a" : "none",
+                        }}
+                      />
+                    </Link>
+                  ) : (
+                    <HiOutlineUserCircle
+                      size={25}
+                      className="hidden 800px:block cursor-pointer dark:text-white text-black"
+                      onClick={() => setOpen(true)}
+                    />
+                  )}
+                  <br />
+                  <br />
+                  <p className="text-[16px] px-2 pl-5 text-black dark:text-white">
+                    Copyright © 2024 Ágora
+                  </p>
+                </div>
+              </div>
+            )}
+          </div>
+          {route === "Login" && (
+            <>
+              {open && (
+                <CustomModal
+                  open={open}
+                  setOpen={setOpen}
+                  setRoute={setRoute}
+                  activeItem={activeItem}
+                  component={Login}
+                  refetch={refetch}
                 />
               )}
-            </div>
-          </div>
+            </>
+          )}
+
+          {route === "Sign-Up" && (
+            <>
+              {open && (
+                <CustomModal
+                  open={open}
+                  setOpen={setOpen}
+                  setRoute={setRoute}
+                  activeItem={activeItem}
+                  component={SignUp}
+                />
+              )}
+            </>
+          )}
+
+          {route === "Verification" && (
+            <>
+              {open && (
+                <CustomModal
+                  open={open}
+                  setOpen={setOpen}
+                  setRoute={setRoute}
+                  activeItem={activeItem}
+                  component={Verification}
+                />
+              )}
+            </>
+          )}
         </div>
-
-        {/* mobile sidebar */}
-        {openSidebar && (
-          <div
-            className="fixed w-full h-screen top-0 left-0 z-[99999] dark:bg-[unset] bg-[#00000024]"
-            onClick={handleClose}
-            id="screen"
-          >
-            <div className="w-[70%] fixed z-[999999999] h-screen bg-white dark:bg-slate-900 dark:bg-opacity-90 top-0 right-0">
-              <NavItems activeItem={activeItem} isMobile={true} />
-
-              <HiOutlineUserCircle
-                size={25}
-                className="hidden 800px:block cursor-pointer dark:text-white text-black"
-                onClick={() => setOpen(true)}
-              />
-
-              <br />
-              <br />
-              <p className="text-[16px] px-2 pl-5 text-black dark:text-white">
-                Copyright © 2024 Ágora
-              </p>
-            </div>
-          </div>
-        )}
-      </div>
-      {route === "Login" && (
-        <>
-          {open && (
-            <CustomModal
-              open={open}
-              setOpen={setOpen}
-              setRoute={setRoute}
-              activeItem={activeItem}
-              component={Login}
-            />
-          )}
-        </>
       )}
-
-      {route === "Sign-Up" && (
-        <>
-          {open && (
-            <CustomModal
-              open={open}
-              setOpen={setOpen}
-              setRoute={setRoute}
-              activeItem={activeItem}
-              component={SignUp}
-            />
-          )}
-        </>
-      )}
-
-      {route === "Verification" && (
-        <>
-          {open && (
-            <CustomModal
-              open={open}
-              setOpen={setOpen}
-              setRoute={setRoute}
-              activeItem={activeItem}
-              component={Verification}
-            />
-          )}
-        </>
-      )}
-    </div>
+    </>
   );
 };
 
