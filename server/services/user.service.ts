c9c874db6b6d8496bf7aea1bb6b4b0874c03e852
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-06-27 10:33:54 -0300

    fix(users): change sort field to createdAt in getAllUsersService

--- server/services/user.service.ts
+++ server/services/user.service.ts
@@ -14,8 +14,8 @@ export const getUserById = async (id: string, res: Response) => {
   }
 };
 
-export const geAllUsersService = async (res: Response) => {
-  const users = await userModel.find().sort({ created: -1 });
+export const getAllUsersService = async (res: Response) => {
+  const users = await userModel.find().sort({ createdAt: -1 });
 
   res.status(201).json({
     success: true,
