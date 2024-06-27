411f5a47301875b80268351afd276a4e12717775
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-06-27 10:38:49 -0300

    refactor(notification): rename updateNotifications to updateNotification

--- server/routes/notification.route.ts
+++ server/routes/notification.route.ts
@@ -2,7 +2,7 @@ import express from "express";
 import { authorizeRoles, isAuthenticated } from "../middleware/auth";
 import {
   getNotifications,
-  updateNotifications,
+  updateNotification,
 } from "../controllers/notification.controller";
 const notificationRoute = express.Router();
 
@@ -12,12 +12,11 @@ notificationRoute.get(
   authorizeRoles("admin"),
   getNotifications
 );
-
 notificationRoute.put(
-  "/get-notification/:id",
+  "/update-notification/:id",
   isAuthenticated,
   authorizeRoles("admin"),
-  updateNotifications
+  updateNotification
 );
 
 export default notificationRoute;
