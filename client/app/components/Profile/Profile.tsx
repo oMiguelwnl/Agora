363a907fda4207d9a561efd80209eed022d980a1
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-06-27 16:07:53 -0300

    feat(password): Add change password component

--- client/app/components/Profile/Profile.tsx
+++ client/app/components/Profile/Profile.tsx
@@ -3,6 +3,7 @@ import SideBarProfile from "./SideBarProfile";
 import { useLogOutQuery } from "@/redux/features/auth/authApi";
 import { signOut } from "next-auth/react";
 import ProfileInfo from "./ProfileInfo";
+import ChangePassword from "./ChangePassword";
 
 type Props = {
   user: any;
@@ -50,6 +51,12 @@ const Profile: FC<Props> = ({ user }) => {
       {active === 1 && (
         <div className="w-full h-full bg-transparent mt-[80px]">
           <ProfileInfo avatar={avatar} user={user} />
+        </div>
+      )}
+
+      {active === 2 && (
+        <div className="w-full h-full bg-transparent mt-[80px]">
+          <ChangePassword />
         </div>
       )}
     </div>
