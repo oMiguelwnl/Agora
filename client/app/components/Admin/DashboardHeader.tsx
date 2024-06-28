9766ba3ed3480d752d5f0492a0834a8aef6e1f1b
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-06-28 11:07:16 -0300

    feat(dashboard): Implement initial dashboard header

---
+++ client/app/components/Admin/DashboardHeader.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+function DashboardHeader({}: Props) {
+  return <div>DashboardHeader</div>;
+}
+
+export default DashboardHeader;
---
+++ client/app/components/Admin/DashboardHero.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import DashboardHeader from "./DashboardHeader";
+
+type Props = {};
+
+function DashboardHero({}: Props) {
+  return (
+    <div>
+      <DashboardHeader />
+    </div>
+  );
+}
+
+export default DashboardHero;
