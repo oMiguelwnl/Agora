0c0736697e444b877b6924cc35e0592a1e45ee85
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-07-04 10:42:39 -0300

    feat: add getAllUsers, updateUserRole and deleteUser Endpoints

--- client/redux/features/user/userApi.ts
+++ client/redux/features/user/userApi.ts
@@ -24,7 +24,32 @@ export const userApi = apiSlice.injectEndpoints({
       query: ({ oldPassword, newPassword }) => ({
         url: "update-user-password",
         method: "PUT",
-        body: { oldPassword, newPassword },
+        body: {
+          oldPassword,
+          newPassword,
+        },
+        credentials: "include" as const,
+      }),
+    }),
+    getAllUsers: builder.query({
+      query: () => ({
+        url: "get-users",
+        method: "GET",
+        credentials: "include" as const,
+      }),
+    }),
+    updateUserRole: builder.mutation({
+      query: ({ email, role }) => ({
+        url: "update-user",
+        method: "PUT",
+        body: { email, role },
+        credentials: "include" as const,
+      }),
+    }),
+    deleteUser: builder.mutation({
+      query: (id) => ({
+        url: `delete-user/${id}`,
+        method: "DELETE",
         credentials: "include" as const,
       }),
     }),
@@ -35,4 +60,7 @@ export const {
   useUpdateAvatarMutation,
   useEditProfileMutation,
   useUpdatePasswordMutation,
+  useGetAllUsersQuery,
+  useUpdateUserRoleMutation,
+  useDeleteUserMutation,
 } = userApi;
