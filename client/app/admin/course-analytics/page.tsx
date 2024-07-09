db28eefa82fba7d3fa2fd06c4e5ffa81e993522c
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-07-09 10:19:13 -0300

    feat: add Course Analytics Component

---
+++ client/app/admin/course-analytics/page.tsx
@@ -0,0 +1,31 @@
+'use client'
+import React from 'react'
+import AdminSidebar from "../../components/Admin/sidebar/AdminSidebar";
+import Heading from '../../../app/utils/Heading';
+import CourseAnalytics from "../../components/Admin/Analytics/CourseAnalytics";
+import DashboardHeader from '../../../app/components/Admin/DashboardHeader';
+
+type Props = {}
+
+const page = (props: Props) => {
+  return (
+    <div>
+        <Heading
+         title="Ágora - Admin"
+         description="Ágora is a platform for students to learn and get help from teachers"
+         keywords="Programing,MERN,Redux,Machine Learning"
+        />
+        <div className="flex">
+            <div className="1500px:w-[16%] w-1/5">
+                <AdminSidebar />
+            </div>
+            <div className="w-[85%]">
+               <DashboardHeader />
+               <CourseAnalytics />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default page
