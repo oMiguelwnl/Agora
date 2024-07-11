a28e3be379c4962020a5dea06bdccbc49de59961
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-07-11 10:57:31 -0300

    feat: add FAQ to the Home Page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -5,6 +5,7 @@ import Header from "./components/Header";
 import Hero from "./components/Route/Hero";
 import Courses from "./components/Route/Courses";
 import Reviews from "./components/Route/Reviews";
+import FAQ from "./components/FAQ/FAQ";
 
 interface Props {}
 
@@ -30,6 +31,7 @@ const Page: FC<Props> = (props) => {
       <Hero />
       <Courses />
       <Reviews />
+      <FAQ/>
     </div>
   );
 };
