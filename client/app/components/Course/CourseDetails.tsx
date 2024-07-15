5faefcc87458b1460d201dac95fdc3b027cbc8d8
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-07-15 10:27:14 -0300

    feat: Add course access control page

--- client/app/components/Course/CourseDetails.tsx
+++ client/app/components/Course/CourseDetails.tsx
@@ -11,7 +11,6 @@ import CheckOutForm from "../Payment/CheckOutForm";
 import { useLoadUserQuery } from "@/redux/features/api/apiSlice";
 import Image from "next/image";
 import { VscVerifiedFilled } from "react-icons/vsc";
-import { loadStripe } from "@stripe/stripe-js";
 
 type Props = {
   data: any;
@@ -21,12 +20,9 @@ type Props = {
   setOpen: any;
 };
 
-const stripePromise = loadStripe(
-  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string
-);
-
 const CourseDetails = ({
   data,
+  stripePromise,
   clientSecret,
   setRoute,
   setOpen: openAuthModal,
