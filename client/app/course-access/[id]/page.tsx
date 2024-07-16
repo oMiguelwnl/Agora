7fb11ed2834ed8d12bf23c6eef6563991460739a
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-07-16 15:03:24 -0300

    refactor: clean up and optimize course access validation logic

--- client/app/course-access/[id]/page.tsx
+++ client/app/course-access/[id]/page.tsx
@@ -1,19 +1,17 @@
 "use client";
-
+import CourseContent from "@/app/components/Course/CourseContent";
 import Loader from "@/app/components/Loader/Loader";
 import { useLoadUserQuery } from "@/redux/features/api/apiSlice";
 import { redirect } from "next/navigation";
 import React, { useEffect } from "react";
-import CourseContent from "../../components/Course/CourseContent";
 
 type Props = {
   params: any;
 };
 
-const page = ({ params }: Props) => {
+const Page = ({ params }: Props) => {
   const id = params.id;
-
-  const { isLoading, error, data } = useLoadUserQuery(undefined, {});
+  const { isLoading, error, data, refetch } = useLoadUserQuery(undefined, {});
 
   useEffect(() => {
     if (data) {
@@ -23,11 +21,12 @@ const page = ({ params }: Props) => {
       if (!isPurchased) {
         redirect("/");
       }
-      if (error) {
-        redirect("/");
-      }
+    }
+    if (error) {
+      redirect("/");
     }
   }, [data, error]);
+
   return (
     <>
       {isLoading ? (
@@ -41,4 +40,4 @@ const page = ({ params }: Props) => {
   );
 };
 
-export default page;
+export default Page;
