df4c44ce963a59143dbce4d260ea5259de4a4b6b
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-07-16 14:40:32 -0300

    feat: add Faq page

---
+++ client/app/faq/page.tsx
@@ -0,0 +1,36 @@
+"use client";
+import React, { useState } from "react";
+import Heading from "../utils/Heading";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import FAQ from "../components/FAQ/FAQ";
+
+type Props = {};
+
+const Page = (props: Props) => {
+  const [open, setOpen] = useState(false);
+  const [activeItem, setActiveItem] = useState(4);
+  const [route, setRoute] = useState("Login");
+
+  return (
+    <div className="min-h-screen">
+      <Heading
+        title="FAQ - Ágora"
+        description="Ágora is a platform for selling courses, providing a wide range of educational content to enhance your skills and knowledge."
+        keywords="Ágora, online courses, course sales, e-learning, education, skill development, learning platform, buy courses"
+      />
+      <Header
+        open={open}
+        setOpen={setOpen}
+        activeItem={activeItem}
+        setRoute={setRoute}
+        route={route}
+      />
+      <br />
+      <FAQ />
+      <Footer />
+    </div>
+  );
+};
+
+export default Page;
